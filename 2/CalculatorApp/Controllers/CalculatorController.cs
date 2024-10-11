09f37d9525009002a466b9781b6f3d6e63a6de90
Namlev <mikipatkowski@protonmail.com>
2024-10-11 09:23:39 +0200

    zadanie 2.1

---
+++ 2/CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using CalculatorApp.Models;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /Calculator/
+        public IActionResult Index()
+        {
+            var model = new CalculatorModel();
+            return View(model);
+        }
+
+        // POST: /Calculator/
+        [HttpPost]
+        public IActionResult Index(CalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Dodaj liczby i ustaw wynik
+                model.Result = model.Number1 + model.Number2;
+            }
+
+            // Przekaż model z wynikiem do widoku
+            return View(model);
+        }
+    }
+}
