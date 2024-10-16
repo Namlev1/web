848974848e3ec4270c45d7f4daa2ab3f36e806f7
kwiatkM <01169586@pw.edu.pl>
2024-10-16 18:34:22 +0200

    quotes

---
+++ 2/CalculatorApp/Controllers/QuoteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class QuoteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
