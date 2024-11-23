fec4cbdd80b42380547e47b815642479542fbf1c
namlev <mikipatkowski@protonmail.com>
2024-11-23 11:55:28 +0100

    Add db docker-compose, basic rest api

---
+++ db/src/main/java/com/example/db/controllers/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.db.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    
+    @GetMapping("/home")
+    String home() {
+        return "home";
+    }
+}
