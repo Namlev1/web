eb5b9526504f13f784b612edebd7836f2ff3ee05
kwiatkM <01169586@pw.edu.pl>
2024-12-28 20:29:28 +0100

    add front

--- projekt/mvc/mvc/Program.cs
+++ projekt/mvc/mvc/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 // Konfiguracja domyślnej trasy
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Task}/{action=Index}/{id?}");
 
 app.Run();
